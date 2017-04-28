90e257493e8f7a2ccd947a39547a6c25dfc1a3e4
Andrew Malkin <andymalkin@hotmail.com>
2017-04-28 03:49:45 -0700

    create an async method to extract the info wanted from the user query

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,27 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-
-var dispalyDoctors = function(practicesArray) {
-  $('#doctor-div').hide();
-
-};
-
 $(document).ready(function() {
   var currentDoctorObject;
   $('#doctor-div').hide();
 
-
   $('#find-doctor').submit(function() {
-    event.preventDefault();
     currentDoctorObject  = new Doctor();
     var symptom = $("#symptom").val();
     var distance = $("#distance").val();
     var sort = $("#sort").val();
     var limit = $("#limit").val();
 
-    currentDoctorObject.findDoctors(symptom, distance, sort, limit, dispalyDoctors);
-
-    $('#doctor-div').show();
+    currentDoctorObject.findDoctors(symptom, distance, sort, limit);
 
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,18 +1,34 @@
 var apiKey = require('./../.env').apiKey;
-var practicesArray = [];
 
 Doctor = function(){
 };
 
-Doctor.prototype.findDoctors = function(symptom, distance, sort, limit, dispalyDoctors) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=' + 45.5231 + ',-' + 122.6765 + ',' + distance + '&sort=' + sort + '&limit=' + limit + '&user_key=' + apiKey)
-   .then(function(response) {
-     for (var i = 0; i < limit; i++) {
-       practicesArray.push(i);
-     }
-     console.log(practicesArray);
-     return practicesArray;
+Doctor.prototype.findDoctors = function(symptom, distance, sort, limit) {
+  var practicesObjArray = [];
+  var nameArray = [];
+  var phoneArray = [];
+  var addressArray = [];
+  var practicesArray = [];
+  var url = "https://api.betterdoctor.com/2016-03-01/doctors?user_location=37.77,-122.41&skip=0&query=" + symptom + "&location=45.5231,-122.6765," + distance + "&sort=" + sort + "&limit=" + limit + "&user_key=" + apiKey;
+
+  $.getJSON(url)
+  .then(function(response) {
+    for (var i = 0; i < response.meta.count; i++) {
+      practicesObjArray.push(response.data[i].practices);
+    }
+    console.log(practicesObjArray);
+    var newArray = practicesObjArray.map(function(array) {
+      for (var i = 0; i < array.length; i++) {
+        nameArray.push(array[i].name);
+        phoneArray.push(array[i].phones[0].number);
+        addressArray.push(array[i].visit_address);
+      }
+    });
+  console.log(nameArray);
+  console.log(phoneArray);
+  console.log(addressArray);
   });
 };
+
 
 exports.doctorModule = Doctor;
